f1da473aa2ffc47b9c166073b6d477af5e952fb0
VShkolnyk <vovashkolnyk@gmail.com>
2020-04-06 14:25:19 +0300

    add lesson 2

---
+++ lesson 2/lesson 2.js
@@ -0,0 +1,72 @@
+
+//let i = prompt('Enter number');
+//
+//if(i>0){
+//console.log ('1');
+//}
+//else if(i<0){
+//console.log ('-1');
+//}
+//else{
+//console.log ('0');
+//}
+
+
+/*
+let name = prompt(`Who's there?`);
+
+
+if(name == 'Admin') {
+	let password = prompt(`Password?`);
+	
+	if (password == null){
+	alert('Canceled');
+	}
+	else if (password == 'TheMaster'){
+		alert('Welcome');
+	}
+	else{
+		alert ('Wrong password');
+	}
+}
+else if (name == null){
+	alert('Canceled');
+}
+else{
+	alert(`I don't know you`);
+}
+*/
+
+
+/*
+let name = prompt(`Who's there?`);
+
+switch (name) {
+	case 'Admin': {
+		let password = prompt(`Password?`);
+		switch (password) {
+			case null: {
+				alert('Canceled');
+				break;
+			}
+			case 'TheMaster': {
+				alert('Welcome');
+				break;
+			}
+			default: {
+				alert('Wrong password');
+				break;
+			}
+		}
+		break;
+	}
+		case null: {
+			alert('Canceled');
+			break;
+		}
+		default: {
+			alert(`I don't know you`);
+			break;
+		}
+}
+*/
