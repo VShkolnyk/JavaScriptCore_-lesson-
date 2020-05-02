4144f758505e9de78e0e221a455280f14cf360df
Volodymyr <45852633+VShkolnyk@users.noreply.github.com>
2020-05-02 09:27:03 +0300

    Update script.js

--- lesson 14/script.js
+++ lesson 14/script.js
@@ -2,7 +2,6 @@ function addText() {
     let element = document.querySelectorAll('ul>li');
     for (let i = 0; i < element.length; i++) {
         element[i].innerHTML = 'some text';
-
-        document.getElementsByTagName('p')[0].innerHTML = 'Кількість елементів li = ' + element.length;
     }
+    document.querySelector('p').innerHTML = 'Кількість елементів li = ' + element.length;
 }
